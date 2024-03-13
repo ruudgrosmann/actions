3885cae4f0c2e555e6b8b5ae2a517c8e6845e8db
rdgon <rdgon@sdu.nl>
2024-03-13 12:57:29 +0100

    zip03

--- zipper.js
+++ zipper.js
@@ -20,58 +20,58 @@ function getDateStamp() {
              tweepos (now.getDate()));
 }
 
+	function getdata (res) {
+		return (fs.readFileSync(res));
+	}
+
 system(`zip -r ${res} bin`).then(output => {
-    console.log(output);
-	console.log( `resultaat: ${res}`);
+		console.log(output);
+		console.log( `resultaat: ${res}`);
+
+	const postData = JSON.stringify({
+		'TextBody': 'Dit zijn de resultaten van de laatste testrun',
+		'HtmlBody': '<p><i>Hello</i> world!</p>',
+		'From': "informatie@sdu.nl",
+		'To': "r.grosmann@sdu.nl",
+		'Subject': "testresultaten",
+		'MessageStream': "outbound",
+		"Attachments": [
+			{	"Name": 'resultaat_' + getDateStamp() + '.zip',
+				"Content": Buffer.from(getdata(res)).toString('base64'),
+				"ContentType": 'application/zip'
+			}
+		]
+	});
+
+	const options = {
+	  hostname: 'api.postmarkapp.com',
+	  port: 443,
+	  path: '/email',
+	  method: 'POST',
+	  headers: {
+		'X-Postmark-Server-Token': process.env.POSTMARK_TOKEN,
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+		'Content-Length': Buffer.byteLength(postData),
+	  },
+	};
+
+	const req = http.request(options, (res) => {
+	  console.log(`STATUS: ${res.statusCode}`);
+	  res.setEncoding('utf8');
+	  res.on('data', (chunk) => {
+		console.log(`resultaat: ${JSON.parse(chunk).Message}`);
+	  });
+	  res.on('end', () => {
+	  });
+	});
+
+	req.on('error', (e) => {
+	  console.error(`problem with request: ${e.message}`);
+	});
+
+	req.write(postData);
+	req.end();
 }).catch(error => {
     console.error(error);
 })
-
-function getdata (res) {
-	return (fs.readFileSync(res));
-}
-
-const postData = JSON.stringify({
-	'TextBody': 'Dit zijn de resultaten van de laatste testrun',
-	'HtmlBody': '<p><i>Hello</i> world!</p>',
-	'From': "informatie@sdu.nl",
-	'To': "r.grosmann@sdu.nl",
-	'Subject': "testresultaten",
-	'MessageStream': "outbound",
-	"Attachments": [
-		{	"Name": 'resultaat_' + getDateStamp() + '.zip',
-			"Content": Buffer.from(getdata(res)).toString('base64'),
-			"ContentType": 'application/zip'
-		}
-	]
-});
-
-const options = {
-  hostname: 'api.postmarkapp.com',
-  port: 443,
-  path: '/email',
-  method: 'POST',
-  headers: {
-	'X-Postmark-Server-Token': process.env.POSTMARK_TOKEN,
-	'Accept': 'application/json',
-	'Content-Type': 'application/json',
-    'Content-Length': Buffer.byteLength(postData),
-  },
-};
-
-const req = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`resultaat: ${JSON.parse(chunk).Message}`);
-  });
-  res.on('end', () => {
-  });
-});
-
-req.on('error', (e) => {
-  console.error(`problem with request: ${e.message}`);
-});
-
-req.write(postData);
-req.end();
